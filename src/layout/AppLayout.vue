0a53226caebe543c941185e42a0027e9e59711b8
Puter <puterpark@me.com>
2026-01-28 11:33:47 +0900

    PP-40 feat: AdFit 연동

--- src/layout/AppLayout.vue
+++ src/layout/AppLayout.vue
@@ -26,10 +26,14 @@
     <AppSidebar />
     <div class="layout-main-container">
       <div class="mb-4 flex items-center justify-center">
-        <Adsense data-ad-client="ca-pub-8002214670454767" data-ad-slot="6356961766" ins-style="display: inline-block; width: 320px; height: 50px" />
+        <ins class="kakao_ad_area" style="display: none" data-ad-unit="DAN-AaSJlRVkpgNkrZjv" data-ad-width="320" data-ad-height="50"></ins>
+        <scriptx src="//t1.daumcdn.net/kas/static/ba.min.js" async />
       </div>
       <div class="layout-main">
         <router-view />
+        <div class="mt-4 flex items-center justify-center">
+          <Adsense data-ad-client="ca-pub-8002214670454767" data-ad-slot="6356961766" ins-style="display: inline-block; width: 320px; height: 50px" />
+        </div>
       </div>
       <AppFooter />
     </div>
