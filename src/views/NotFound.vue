aa2976ae50317cfd5fb0058ca9e80645d4ffa4ba
Puter <puterpark@me.com>
2026-02-11 23:04:40 +0900

    refactor: NotFound - 404 클릭 시 단축링크 접근

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,4 +1,15 @@
-<script setup></script>
+<script setup>
+  import { useRoute } from 'vue-router';
+
+  const route = useRoute();
+
+  const goExternal = () => {
+    const orgUri = route.fullPath;
+    if (orgUri !== '/error/404') {
+      window.location.href = `https://puter.us/s${orgUri}`;
+    }
+  };
+</script>
 
 <template>
   <div class="flex min-h-screen items-center justify-center overflow-hidden">
@@ -6,7 +17,7 @@
       <img src="https://cdn.puter.us/images/apple-touch-icon.png" alt="" class="mb-8 w-16 rounded-md" />
       <div style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, color-mix(in srgb, var(--primary-color), transparent 60%) 10%, var(--surface-ground) 30%)">
         <div class="bg-surface-0 dark:bg-surface-900 flex w-full flex-col items-center px-8 py-20 sm:px-20" style="border-radius: 53px">
-          <span class="text-primary text-3xl font-bold">404</span>
+          <span class="text-primary text-3xl font-bold" @click="goExternal">404</span>
           <h1 class="text-surface-900 dark:text-surface-0 mb-2 text-3xl font-bold lg:text-5xl">Not Found</h1>
           <div class="text-surface-600 dark:text-surface-200 mb-8">Requested resource is not available.</div>
           <Button label="Go Back" @click="$router.back()" />
