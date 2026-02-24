016aafd9a44bafffb5e52f6ca6bda87013230aa2
Puter <puterpark@me.com>
2026-02-24 12:30:11 +0900

    refactor: UnixTime
    - unixTime 클릭 시 복사

--- src/views/tools/UnixTime.vue
+++ src/views/tools/UnixTime.vue
@@ -1,13 +1,17 @@
 <script setup>
   import { computed, onMounted, onUnmounted, ref } from 'vue';
   import dayjs from 'dayjs';
+  import { useAppToast } from '@/composables/useAppToast';
   import { useSeo } from '@/composables/useSeo';
+  import { copyText } from '@/utils/commonUtils';
 
   useSeo({
     title: 'UnixTime',
     description: 'unixtime를 변환해 보세요.',
     keywords: 'unixtime, 유닉스타임, 변환, convert, converter',
   });
+
+  const toast = useAppToast();
 
   const intervalUnixTime = ref(dayjs().unix());
   const intervalRealTime = ref(dayjs.unix(intervalUnixTime.value).format('YYYY-MM-DD HH:mm:ss'));
@@ -42,6 +46,13 @@
   onUnmounted(() => {
     clearInterval(timer);
   });
+
+  const handleCopy = async (text) => {
+    const isSuccess = await copyText(text);
+    if (isSuccess) {
+      toast.success('클립보드에 복사되었습니다.');
+    }
+  };
 </script>
 
 <template>
@@ -49,7 +60,7 @@
     <div class="card flex w-full flex-col gap-4">
       <div class="text-xl font-semibold">UnixTime</div>
       <div class="flex flex-col items-center md:items-start">
-        <div class="text-5xl font-black">{{ intervalUnixTime }}</div>
+        <div class="text-5xl font-black" @click="handleCopy(intervalUnixTime)">{{ intervalUnixTime }}</div>
         <div class="text-2xl font-black md:ml-6">{{ intervalRealTime }}</div>
       </div>
     </div>
